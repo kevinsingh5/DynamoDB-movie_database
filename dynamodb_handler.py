57b8f85717394d9556478546d495aeae69479b44
Kevin Singh <singhk7@yahoo.com>
2018-03-22 23:26:25 -0500

    fixed 'ResourceInUseException' bug

--- dynamodb_handler.py
+++ dynamodb_handler.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import boto3
 import json
+import decimal
 import sys
 
 # Note - Do not change the class name and constructor
@@ -9,7 +10,8 @@ class DynamoDBHandler:
 
     def __init__(self, region):
         self.client = boto3.client('dynamodb')
-        self.resource = boto3.resource('dynamodb', region_name=region)
+        self.resource = boto3.resource('dynamodb', region_name=region, endpoint_url='http://localhost:8000')
+        # self.resource = boto3.resource('dynamodb',endpoint_url="http://localhost:8000") 
 
     def help(self):
         print('Supported Commands:')
@@ -21,9 +23,71 @@ class DynamoDBHandler:
         print('6. print_stats')
         print('7. delete_table')
 
+    # TODO: create query function for python2/3 input
+
     def create_and_load_data(self, tableName, fileName):
         # TODO - This function should create a table named <tableName> 
         # and load data from the file named <fileName>
+        table = self.resource.create_table(
+            TableName = tableName,
+            KeySchema=[
+                {
+                    'AttributeName': 'year',
+                    'KeyType': 'HASH' #Partition key
+                },
+                {
+                    'AttributeName': 'title',
+                    'KeyType': 'RANGE' #Sort key
+                }
+            ], 
+            AttributeDefinitions=[
+                {
+                    'AttributeName': 'year',
+                    'AttributeType': 'N'
+                },
+                {
+                    'AttributeName': 'title',
+                    'AttributeType': 'S'
+                },
+
+            ],
+            ProvisionedThroughput={
+                'ReadCapacityUnits': 10,
+                'WriteCapacityUnits': 10
+            }
+        )
+        print('Created table')
+        # Wait until the table exists.
+        #table.meta.client.get_waiter('table_exists').wait(TableName=tableName)
+        #print('Table status', table.item_count)
+        '''waiter = self.client.get_waiter('table_exists')
+        waiter.wait(
+            TableName=tableName,
+            #WaiterConfig={
+             #   'Delay': 50,
+              #  'MaxAttempts': 5
+            #}
+        )'''
+
+        table = self.resource.Table(tableName)
+        
+        with open(fileName) as json_file:
+            movies = json.load(json_file, parse_float = decimal.Decimal)
+            for movie in movies:
+                year = int(movie['year'])
+                title = movie['title']
+                info = movie['info']
+
+                print('Adding movie:', year, title)
+
+                table.put_item(
+                    Item={
+                        'year': year,
+                        'title': title,
+                        'info': info,
+                    }
+                )
+
         return None
 
 
@@ -35,10 +99,18 @@ class DynamoDBHandler:
         # For commands 'search_movie_actor', 'search_movie_actor_director', print_stats' :
         #       return the a list of json objects where each json object has only the required
         #       keys and attributes of the expected result items.
-
         # Note: You should not print anything to the command line in this function.
-        response = None
+        command = command_string.split(' ')
+        response = ''
 
+        if command[0] == 'insert_movie':
+            year = raw_input('Year> ')
+            title = raw_input('Title> ')
+            director = raw_input('Director> ')
+            actors = raw_input('Actors> ')
+            release_date = raw_input('Release Date> ')
+            rating = raw_input('Rating> ')
+            
         return response
 
 def main():
@@ -52,6 +124,12 @@ def main():
 
     # initialize handler with specified region
     dynamodb_handler = DynamoDBHandler(region)
+
+    # Check if table/data has already been loaded
+    try:
+        dynamodb_handler.create_and_load_data('Movies', 'moviedata.json')   # create table and laod data from file
+    except dynamodb_handler.client.exceptions.ResourceInUseException:
+        print('Table \'Movies\' already exists')
 
     while True:
         try:
